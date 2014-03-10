e1e70fd84bce3fcec7b92ee2968c0ec0157f16f8
Duarte Miguel Tavares Barreira <ist164752@rnlmaster.rnl.ist.utl.pt>
2014-03-10 13:14:14 +0000

    First commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+
+/* Constants */
+#define UNDEFINED -1
+
+/* Output */
+/*  número de grupos máximos de pessoas que partilham informação; */
+unsigned int groups = 0;
+/* Uma linha com o tamanho do maior grupo máximo de pessoas que partilham informação; */
+unsigned int biggest = 0;
+/* número de grupos máximos de pessoas que partilham informação apenas dentro do grupo; */
+unsigned int own_group_only = 0;
+
+
+int main(int argc, char *argv[])
+{
+    exit(EXIT_SUCCESS);
+}
