c3123433f6759d6607e50ea7cddf80e99edec02a
Duarte Miguel Tavares Barreira <ist164752@rnlmaster.rnl.ist.utl.pt>
2014-03-13 17:18:06 +0000

    cenas

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
-#include <queue>
+#include <stack>
 #include <list>
+#include <array>
+
+//using namespace std;
 
 /* Constants */
 #define UNDEFINED -1
@@ -15,16 +18,47 @@ unsigned int biggest = 0;
 /* número de grupos máximos de pessoas que partilham informação apenas dentro do grupo; */
 unsigned int own_group_only = 0;
 
-void visit(/* vertex u)*/);
+class Vertex {
+    public:
+        Vertex() : d(UNDEFINED) {}
+        Vertex(unsigned int _id) : id(_id), d(UNDEFINED), low(UNDEFINED) {}
+        Vertex(unsigned int _id, unsigned int _d, unsigned int _low) :
+            id(_id), d(_d), low(_low) {}
+        virtual ~Vertex() {}
 
-void SCC_Tarjan(/* *Graph g */)
+        unsigned int Id() const { return id; }
+        unsigned int D() const { return d; }
+        unsigned int Low() const { return low; }
+    private:
+        unsigned int id;
+        unsigned int d;
+        unsigned int low;
+        //this will work as an adjacency list
+        std::list<Vertex*> adjacents;
+};
+
+void visit(Vertex *v);
+
+//void SCC_Tarjan(std::vector<std::list<Vertex*> *graph)
+void SCC_Tarjan(std::vector<Vertex*> graph)
 {
+    //The counter for the amount of vertices visited
     unsigned int visited = 0;
-    //std::queue<G>;
+    // The stack needed by the algorithm
+    std::stack<Vertex*> L;
+    // We need to know if a given vertex is on the stack
+    //std::array<bool, graph.size()> onStack;
+    //bool[graph->size()] onStack; //V is the number of vertices on the graph;
+
+    for(Vertex* v : graph) {
+        if(v->D() == UNDEFINED) {
+            visit(v);
+        }
+    }
 }
 
 
-void visit()
+void visit(Vertex *v)
 {
     
 }
