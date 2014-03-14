d4ccbf99f47ea11188f71b170a4b7fe1713d5dc8
Duarte Miguel Tavares Barreira <ist164752@rnlmaster.rnl.ist.utl.pt>
2014-03-14 13:01:43 +0000

    Added the algorith

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <cstdio>
-#include <cstdlib>
-#include <vector>
-#include <stack>
+#include <cstdio> //scanf
+#include <cstdlib> // exit
+#include <algorithm> // min
 #include <list>
-#include <array>
+#include <stack>
+#include <vector>
 
 //using namespace std;
 
@@ -20,24 +20,34 @@ unsigned int own_group_only = 0;
 
 class Vertex {
     public:
-        Vertex() : d(UNDEFINED) {}
+        Vertex() : id(0), d(UNDEFINED), low(UNDEFINED) {}
         Vertex(unsigned int _id) : id(_id), d(UNDEFINED), low(UNDEFINED) {}
         Vertex(unsigned int _id, unsigned int _d, unsigned int _low) :
-            id(_id), d(_d), low(_low) {}
+            id(_id), d(_d), low(_low), onStack(false) {}
         virtual ~Vertex() {}
 
         unsigned int Id() const { return id; }
         unsigned int D() const { return d; }
         unsigned int Low() const { return low; }
+        bool OnStack() const { return onStack; }
+        std::list<Vertex*> Adjacents() const { return adjacents; }
+
+        void setId(unsigned int _id) { id = _id; }
+        void setD(unsigned int _d) { d = _d; }
+        void setLow(unsigned int _low) { low = _low; }
+        void setOnStack(bool b) { onStack = b; }
+
     private:
         unsigned int id;
         unsigned int d;
         unsigned int low;
+        // To test if this vertex was already added to the stack
+        bool onStack;
         //this will work as an adjacency list
         std::list<Vertex*> adjacents;
 };
 
-void visit(Vertex *v);
+void visit(Vertex *v, std::stack<Vertex*> *s, unsigned int *visited);
 
 //void SCC_Tarjan(std::vector<std::list<Vertex*> *graph)
 void SCC_Tarjan(std::vector<Vertex*> graph)
@@ -46,21 +56,41 @@ void SCC_Tarjan(std::vector<Vertex*> graph)
     unsigned int visited = 0;
     // The stack needed by the algorithm
     std::stack<Vertex*> L;
-    // We need to know if a given vertex is on the stack
-    //std::array<bool, graph.size()> onStack;
-    //bool[graph->size()] onStack; //V is the number of vertices on the graph;
 
-    for(Vertex* v : graph) {
+    for(Vertex* v : graph) { // corrigir
         if(v->D() == UNDEFINED) {
-            visit(v);
+            visit(v, &L, &visited);
         }
     }
 }
 
 
-void visit(Vertex *v)
+void visit(Vertex *u, std::stack<Vertex*> *s, unsigned int *visited)
 {
-    
+    //std::list<Vertex*>::iterator it = u->Adjacents().begin();
+    u->setD(*visited);
+    u->setLow(*visited);
+    ++(*visited); //*visited += 1;
+    s->push(u);
+
+    for(Vertex *v : u->Adjacents()) {
+        if((v->D() == UNDEFINED) || v->OnStack()) {
+            if(v->D() == UNDEFINED) {
+                visit(v, s, visited);
+            }
+            u->setLow(std::min(u->Low(), v->Low()));
+        }
+    }
+    if(u->D() == u->Low()){
+        do {
+            /*
+             * Adicionar os vertices que eu tiro do stack para um vector
+             * em que cada vector é um scc
+             */
+            //vx = s->pop();
+        }
+        while(true);
+    }
 }
 
 int main(int argc, char *argv[])
